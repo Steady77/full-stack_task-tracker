02024b68658e37247f48800770cfd9369537e117
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-03-16 15:10:30 +0300

    feat: add utils

---
+++ client/src/shared/utils/error-catch.ts
@@ -0,0 +1,11 @@
+export const errorCatch = (error: any): string => {
+	if (error.response && error.response.data) {
+		if (typeof error.response.data.message === 'object') {
+			return error.response.data.message[0];
+		} else {
+			return error.response.data.message;
+		}
+	} else {
+		return error.message;
+	}
+};
